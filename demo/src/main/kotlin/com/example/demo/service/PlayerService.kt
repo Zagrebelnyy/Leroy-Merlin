c532d28bc3b7006b617bd222bd98329671818c3f
Zagrebelnyy <55534963+Zagrebelnyy@users.noreply.github.com>
2022-06-26 20:45:53 +0300

    Delete demo directory

--- demo/src/main/kotlin/com/example/demo/service/PlayerService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service
-
-import com.example.demo.domain.TablePoint
-import org.springframework.stereotype.Service
-
-
-interface PlayerService {
-    fun hit(): TablePoint
-}
